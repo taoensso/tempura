afdd91004647a507341f7be9d6abc4cd6d3cce47
Peter Taoussanis <ptaoussanis@gmail.com>
2020-01-01 10:49:49 +0100

    Bump deps

--- project.clj
+++ project.clj
@@ -12,12 +12,12 @@
 
   :dependencies
   [[org.clojure/clojure "1.7.0"]
-   [com.taoensso/encore "2.94.0"]]
+   [com.taoensso/encore "2.118.0"]]
 
   :plugins
   [[lein-pprint    "1.2.0"]
    [lein-ancient   "0.6.15"]
-   [lein-codox     "0.10.3"]
+   [lein-codox     "0.10.7"]
    [lein-cljsbuild "1.1.7"]]
 
   :profiles
@@ -25,8 +25,9 @@
    :server-jvm {:jvm-opts ^:replace ["-server"]}
    :1.8      {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9      {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :test     {:dependencies [[org.clojure/test.check "0.9.0"]]}
-   :provided {:dependencies [[org.clojure/clojurescript "1.10.145"]]}
+   :1.10     {:dependencies [[org.clojure/clojure "1.10.1"]]}
+   :test     {:dependencies [[org.clojure/test.check "0.10.0"]]}
+   :provided {:dependencies [[org.clojure/clojurescript "1.10.597"]]}
    :dev [:1.9 :test :server-jvm]}
 
   :cljsbuild
